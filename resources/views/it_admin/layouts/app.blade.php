60a9617165c69545f1f96fe27b52e1d09a2de4a0
unknown <dimastm45@gmail.com>
2023-11-09 10:26:27 +0700

    resolved bug on navbar cannot clicked removing local jquery integration from AdminLTE

--- resources/views/it_admin/layouts/app.blade.php
+++ resources/views/it_admin/layouts/app.blade.php
@@ -81,9 +81,13 @@
 
     @vite('resources/js/app.js')
     <!-- jQuery -->
-    <script src="{{ asset('js/jquery/jquery.min.js') }}"></script>
+    {{-- <script src="{{ asset('js/jquery/jquery.min.js') }}"></script> --}}
     <!-- jQuery UI 1.11.4 -->
     <script src="{{ asset('js/jquery-ui/jquery-ui.min.js') }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    {{-- <script>
+        $.widget.bridge('uibutton', $.ui.button)
+    </script> --}}
     <!-- Bootstrap 4 -->
     <script src="{{ asset('js/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
